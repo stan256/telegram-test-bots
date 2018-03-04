425621fcb6e7b1d434b240d639103befeb96b261
Stanislav Studzinskyi <stanislavstudzinskyi@gmail.com>
2018-03-04 15:08:36 +0100

    Main methods for all bots except emoji cleaned

--- src/main/java/com/github/sstdz/mytelegramtestbots/echobot/Main.java
+++ src/main/java/com/github/sstdz/mytelegramtestbots/echobot/Main.java
@@ -6,14 +6,14 @@ import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
 public class Main {
-    public static void main(String[] args) {
-        ApiContextInitializer.init();
-        TelegramBotsApi botsApi = new TelegramBotsApi();
-
-        try {
-            botsApi.registerBot(new SimpleEchoBot());
-        } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//        ApiContextInitializer.init();
+//        TelegramBotsApi botsApi = new TelegramBotsApi();
+//
+//        try {
+//            botsApi.registerBot(new SimpleEchoBot());
+//        } catch (TelegramApiRequestException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
